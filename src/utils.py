4d1b58280c2e4dad10ede35ae8f6c99dab029f85
Diego Cotacio <diego.cotacio@correounivalle.edu.co>
2023-07-22 18:21:41 -0500

    Prediction api

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import dill
 
 import pandas as pd
 import numpy as np
+import pickle
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
@@ -48,5 +49,14 @@ def evaluate_model(X_train, y_train, X_test, y_test, models, params):
         
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
