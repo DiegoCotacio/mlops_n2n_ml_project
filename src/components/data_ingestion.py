9253da1b066696fe0e73b773a39d70a9f155c616
Diego Cotacio <diego.cotacio@correounivalle.edu.co>
2023-07-25 21:18:41 -0500

    update

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,7 +12,7 @@ from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
     test_data_path: str = os.path.join('artifacts', "test.csv")
-    raw_data_path: str = os.path.join('artifacs', "data.csv")
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
 
 
 
@@ -54,9 +54,9 @@ if __name__=="__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
-    data_transformation = DataTransformation()
-    train_array, test_array,_= data_transformation.initiate_data_transformation(train_data, test_data)
+    #data_transformation = DataTransformation()
+    #train_array, test_array,_= data_transformation.initiate_data_transformation(train_data, test_data)
 
-    modeltrainer = ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_array, test_array)) #return r2_score
+    #modeltrainer = ModelTrainer()
+    #print(modeltrainer.initiate_model_trainer(train_array, test_array)) #return r2_score
 
